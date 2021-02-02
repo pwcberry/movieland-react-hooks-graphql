0f9af2649e7a78f942cd3030b5b90122277b61c0
Peter Berry <pwcberry@gmail.com>
2021-02-02 22:05:41 +1100

    Tidy up GraphQL client

--- app/services/graphql.js
+++ app/services/graphql.js
@@ -1,10 +1,8 @@
-import { InMemoryCache, ApolloClient, HttpLink } from "@apollo/client";
+import { InMemoryCache, ApolloClient } from "@apollo/client";
 import { API_URL } from "../constants.js";
 
-console.log("API_URL: '%s'", API_URL);
-
 const client = new ApolloClient({
-    link: new HttpLink({ uri: "http://localhost:4001/graphql" }),
+    uri: `${API_URL}/graphql`,
     cache: new InMemoryCache(),
 });
 
